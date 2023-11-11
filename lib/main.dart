e448df094058287ec89bbb6a768e498e35a4c28f
SantiagusOK <santiagus315@gmail.com>
2023-11-11 19:17:16 -0300

    se agregaron botones de aumentar valores

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Temporizador',
       home: MyHomePage(),
     );
   }
@@ -24,48 +22,103 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int min = 00;
+  int seg = 00;
+  double letra_size = 113;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(50),
-        child: Center(
-          child: Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Row(
-                children: [
-                  Expanded(
-                      child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              labelText: '00',
-                              labelStyle: TextStyle(fontSize: 100)))),
-                  Text(' : ', style: TextStyle(fontSize: 80)),
-                  Expanded(
-                      child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              labelText: '00',
-                              labelStyle: TextStyle(fontSize: 100)))),
-                ],
-              ),
-              const SizedBox(height: 50),
               ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(150, 150),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100))),
-                child: const Text(
-                  'INICIAR',
-                  style: TextStyle(fontSize: 20),
-                ),
-              )
+                  onPressed: () {
+                    aumentarValores('m++');
+                  },
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 100)),
+                  child: Icon(Icons.keyboard_double_arrow_up)),
+              SizedBox(width: 65),
+              ElevatedButton(
+                  onPressed: () {
+                    aumentarValores('s++');
+                  },
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 100)),
+                  child: Icon(Icons.keyboard_double_arrow_up))
             ],
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 130,
+                child: Center(
+                  child: Text('$min', style: TextStyle(fontSize: letra_size)),
+                ),
+              ),
+              Text(':', style: TextStyle(fontSize: letra_size)),
+              SizedBox(
+                  width: 130,
+                  child: Center(
+                    child: Text('$seg', style: TextStyle(fontSize: letra_size)),
+                  )),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    aumentarValores('m--');
+                  },
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 100)),
+                  child: const Icon(Icons.keyboard_double_arrow_down)),
+              const SizedBox(width: 65),
+              ElevatedButton(
+                  onPressed: () {
+                    aumentarValores('s--');
+                  },
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 100)),
+                  child: const Icon(Icons.keyboard_double_arrow_down))
+            ],
+          ),
+        ],
       ),
     );
+  }
+
+  void aumentarValores(String d) {
+    setState(() {
+      switch (d) {
+        case 's++':
+          seg = seg + 5;
+          if (seg >= 60) {
+            seg = 59;
+          }
+        case 's--':
+          seg = seg - 5;
+          if (seg < 0) {
+            seg = 0;
+          }
+          if (seg == 54) {
+            seg = 55;
+          }
+        case 'm++':
+          min = min + 5;
+          if (min >= 60) {
+            min = 59;
+          }
+        case 'm--':
+          min = min - 5;
+          if (min <= 0) {
+            min = 0;
+          }
+          if (min == 54) {
+            min = 55;
+          }
+      }
+    });
   }
 }
