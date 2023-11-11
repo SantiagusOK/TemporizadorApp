4d24e471ead44fb121edcb4a2b6596f9d791b0f6
SantiagusOK <santiagus315@gmail.com>
2023-11-10 21:23:16 -0300

    primer commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# contador
+# flutter_application_1
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,71 @@
-import 'package:contador/rutas.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Ejemplos 2do TSDdS IES 9-010',
-      routerConfig: rutasPaginas,
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(50),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: '00',
+                              labelStyle: TextStyle(fontSize: 100)))),
+                  Text(' : ', style: TextStyle(fontSize: 80)),
+                  Expanded(
+                      child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: '00',
+                              labelStyle: TextStyle(fontSize: 100)))),
+                ],
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(150, 150),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100))),
+                child: const Text(
+                  'INICIAR',
+                  style: TextStyle(fontSize: 20),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
